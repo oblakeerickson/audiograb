b0a725bb9375bc6930143e842b8664e8c06fbfb2
Blake Erickson <o.blakeerickson@gmail.com>
2013-09-08 07:22:58 -0600

    script adds songs to playlist now

--- dl.rb
+++ dl.rb
@@ -1,4 +1,10 @@
+require 'fileutils'
+
 f = File.open('list.txt').to_a
 f.each do |a|
 	system( "youtube-dl -k --extract-audio --audio-format mp3 #{a}" )
+	system( "rm *.mp4" )
+	system( "rm *.flv" )
+	system( "mv *.mp3 process/" )
+  system( "mv process/*.mp3 ~/Music/iTunes/iTunes\\ Media/Automatically\\ Add\\ to\\ iTunes.localized" )
 end
